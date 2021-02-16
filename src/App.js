43c9dc015943643321f76dddc45c01860fbe02cb
Emmanuel Egomson <fegomson@gmail.com>
2021-02-16 17:25:04 -0600

    Added react routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import About from "./components/About";
 import AddTask from "./components/AddTask";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -85,18 +87,26 @@ const App = () => {
   }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {
-        showAddTask && <AddTask onAdd={addTask}/>
-      }
-      {
-        tasks.length > 0 ? 
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 
-        <div className="no-task">No task to display</div>
-      }
-      <Footer/>
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+        
+        <Route path="/" exact render={() => (
+          <>
+            {
+            showAddTask && <AddTask onAdd={addTask}/>
+            }
+            {
+              tasks.length > 0 ? 
+              <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 
+              <div className="no-task">No task to display</div>
+            }
+          </>
+        )}/>
+        <Route path="/about" component={About}/>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   const date = new Date()
@@ -6,7 +7,8 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <p>&copy; {year}</p>
+        <p>Copyright &copy; {year}</p>
+        <Link to="/about">About</Link>
       </footer>
     </>
   )
